6af5ce8e185889b920cc168e094c988e6bd33a3b
Yan Carlos <yan312005@gmail.com>
2025-05-30 23:22:43 -0300

    remove: .exe archives

--- C/8 - Structs/E.D Felipe/Exercicios/Ex. Fixação/q.7.c
+++ C/8 - Structs/E.D Felipe/Exercicios/Ex. Fixação/q.7.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Escreva um programa que declare uma função que some dois números. 
-// Em seguida, declare um ponteiro para essa função e use o ponteiro para chamar a função e imprimir o resultado.
+// Escreva um programa que declare uma função que some dois números.
+// Em seguida, declare um ponteiro para essa função e use o ponteiro para chamar
+// a função e imprimir o resultado.
 
-void soma(int *a, int *b) {
-    *a = *a + *b;
-}
+int soma(int a, int b) { return a + b; }
 
 int main() {
-    void (*pSoma)(int*, int*) = soma;
-    int x = 1, y = 2;
-    pSoma(&x , &y);
-    
-    printf("resultado = %d", x);
-    return 0;
+  int (*pSoma)(int, int) = soma;
+  int x = 1, y = 2;
+
+  printf("resultado = %d", pSoma(x, y));
+  return 0;
 }
